29e0510f1a14ba0524a733dfd8302ec25e231c33
Nahuel <nahuel.defosse@gmail.com>
2011-09-24 19:10:50 -0300

    Comenzando a cablear la funcionalidad

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -7,5 +7,17 @@ class MainWindow(Ui_MainWindow, QMainWindow):
         super(MainWindow, self).__init__()
         self.setupUi(self)
 
+        self.connectSignals()
 
-        
+    def connectSignals(self):
+        self.action_Open_Image.triggered.connect(self.openFile)
+
+    def openFile(self):
+        startFolder = ""
+        f = QFileDialog.getOpenFileName(self,
+                                    self.trUtf8("Please chosse an image to open"),
+                                    startFolder,
+                                    self.trUtf8("Images (*.png *.xpm *.jpg *.tiff *.bmp);; Anything (*.*);;"))
+        print f
+
+    

2a88eb58496097b1626be8e26bf12f831fb025cd
Nahuel <nahuel.defosse@gmail.com>
2011-09-24 19:03:29 -0300

    Agregar el main

---
+++ main.py
@@ -0,0 +1,12 @@
+from PyQt4.QtGui import QApplication
+from gui import MainWindow
+import sys
+def main(argv = sys.argv):
+    app = QApplication(argv)
+    win = MainWindow()
+    win.show()
+    return app.exec_()
+
+if __name__ == "__main__":
+    sys.exit(main())
+    
